4c086b9ac29a4984ea23f5e2a5c6f74c7fd48451
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-11 21:22:16 -0800

    Initial Commit

---
+++ iOSChatServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOSChatServer
+//
+//  Created by Jeremy Hartmann on 2016-03-11.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
