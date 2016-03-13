c2637e5fe5c5e7bc9bb798d0727f20518ad3fcab
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-12 21:29:50 -0800

    Add: StreamHandle Obj-c class.

---
+++ iOSChatServer/StreamHandle.h
@@ -0,0 +1,13 @@
+//
+//  StreamHandle.h
+//  iOSChatServer
+//
+//  Created by Jeremy Hartmann on 2016-03-12.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StreamHandle : NSObject
+
+@end
---
+++ iOSChatServer/StreamHandle.m
@@ -0,0 +1,13 @@
+//
+//  StreamHandle.m
+//  iOSChatServer
+//
+//  Created by Jeremy Hartmann on 2016-03-12.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "StreamHandle.h"
+
+@implementation StreamHandle
+
+@end
