59ba876d6c0d8b5c9714e5b7ece0a28ceb6e17f9
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-14 18:26:38 -0700

    Add: TCPServer class

---
+++ iOSChatServer/TCPServer.h
@@ -0,0 +1,13 @@
+//
+//  TCPServer.h
+//  iOSChatServer
+//
+//  Created by Jeremy Hartmann on 2016-03-14.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCPServer : NSObject
+
+@end
---
+++ iOSChatServer/TCPServer.m
@@ -0,0 +1,13 @@
+//
+//  TCPServer.m
+//  iOSChatServer
+//
+//  Created by Jeremy Hartmann on 2016-03-14.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "TCPServer.h"
+
+@implementation TCPServer
+
+@end
