54e105c1eed5a3bce127d485de92d028283eda56
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-16 00:03:52 -0700

    Add: Command line argument, clean up comments

--- iOSChatServer/main.m
+++ iOSChatServer/main.m
@@ -41,51 +41,23 @@
 
 
 #pragma mark Main
-void connectionHandle (CFSocketRef sref, CFSocketCallBackType callBackType, CFDataRef address, const void *data, void *info)
-{
-//    if (callBackType == kCFSocketAcceptCallBack)
-//    {
-//        NSLog(@"SOMETHING IS HAPPENING!");
-//        
-//        // Create Read and Write Streams CF
-//        CFWriteStreamRef writeStream;
-//        CFReadStreamRef readStream;
-//        
-//        // Pair with socket
-//        CFStreamCreatePairWithSocket(kCFAllocatorDefault, (CFSocketNativeHandle) data, &readStream, &writeStream);
-//        
-//        // Cast to NS Stream
-//        NSInputStream *inStream = (__bridge_transfer NSInputStream *) readStream;
-//        NSOutputStream *outStream = (__bridge_transfer NSOutputStream *)writeStream;
-//        
-//        StreamHandle *streamHandle = [[StreamHandle alloc] initWithStreams:inStream outputStream:outStream];
-//        
-//        [streamHandle.iStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-//        [streamHandle.oStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-//        
-//        [streamHandle.iStream open];
-//        [streamHandle.oStream open];
-//        
-//        while (streamHandle != nil) {
-//            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
-//        }
-//    }
-}
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"iOSChatServer Start");
         
-//        if (argc < 2)
-//        {
-//            NSLog(@"Provide Port number <1-65534>");
-//            return 1;
-//        }
+        if (argc < 2)
+        {
+            NSLog(@"Provide Port number <1-65534>");
+            return 1;
+        }
         
         Main *mainObj = [[Main alloc]init];
         
-//        NSInteger port = atoi(argv[1]);
-        [mainObj runServerOnPort:12345];
+        NSInteger port = atoi(argv[1]);
+        NSLog(@"Running on port: %li", port);
+        
+        [mainObj runServerOnPort:port];
     }
     return 0;
 }
